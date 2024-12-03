64b6a1896704df0598c37bd83460c3851cb1ff68
ljes223 <146264517+ljes223@users.noreply.github.com>
2024-12-03 16:16:04 -0500

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
+<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+<div id="primary">
+    <div id="search-container" role="search">
+        <h1>Search</h1>
+        <?php $showAdvanced = ((get_theme_option('use_advanced_search') !== null) && (get_theme_option('use_advanced_search') == 1)) ? true : false; ?>
+        <?php echo search_form(array('show_advanced' => $showAdvanced)); ?>
+    </div>
+</div><!-- end primary -->
+
+<div id="secondary">
+
+    <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
+    <p><?php echo $homepageText; ?></p>
+    <?php endif; ?>
+    <?php if (get_theme_option('Display Featured Item') == 1): ?>
+    <!-- Featured Item -->
+    <div id="featured-item" class="featured">
+        <h2><?php echo __('Featured Item'); ?></h2>
+        <?php echo random_featured_items(1); ?>
+    </div><!--end featured-item-->
+    <?php endif; ?>
+    <?php if (get_theme_option('Display Featured Collection')): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection" class="featured">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+    <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
+    <!-- Featured Exhibit -->
+    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+    <?php endif; ?>
+</div><!-- end secondary -->
+</div><!--end content -->
+<?php echo foot(); ?>
 
